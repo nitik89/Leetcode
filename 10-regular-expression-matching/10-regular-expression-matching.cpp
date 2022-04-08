d0ffe75597725273d77d0f0ab23a81b4b64f8b94
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-08 05:42:41 +0530

    Time: 3 ms (90.54%), Space: 6.3 MB (83.88%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        bool dp[p.size()+1][s.size()+1];
+        
+        for(int i=0;i<=p.size();i++){
+            for(int j=0;j<=s.size();j++){
+                if(i==0&&j==0){
+                    dp[i][j]=true;
+                }
+                else if(i==0){
+                    dp[i][j]=false;
+                }
+                else if(j==0){
+                    if(p[i-1]=='*'){
+                        dp[i][j]=dp[i-2][j];
+                    }
+                    else{
+                    dp[i][j]=false;
+                    }
+                }
+                else{
+                    if(p[i-1]=='*'){
+                        dp[i][j]=dp[i-2][j];
+                        if(p[i-2]=='.'||p[i-2]==s[j-1]){
+                            dp[i][j]=dp[i][j-1]||dp[i][j];
+                        }
+                        
+                    }
+                    else if(p[i-1]==s[j-1]){
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+                    else if(p[i-1]=='.'){
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j]=false;
+                    }
+                }
+            }
+        }
+        return dp[p.size()][s.size()];
+    }
+};

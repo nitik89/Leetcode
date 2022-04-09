85593d4559ead1925a4f65e8c5874615d4803e09
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-10 04:10:02 +0530

    Time: 96 ms (58.10%), Space: 20 MB (54.56%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,78 +1,108 @@
+bool isValid(int row, int col, int m, int n)
+{
+
+if (row < 0 || col < 0 || row >= m || col >= n)
+    return false;
+
+return true;
+}
+
 class Solution {
 public:
-    int minCost(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();
-        vector<vector<int>>dist(n,vector<int>(m,1e9));
-        
-        queue<pair<int,int>>q;
-        dist[0][0]=0;
-        q.push({0,0});
-        while(!q.empty()){
-            int i=q.front().first;
-            int j=q.front().second;
-            // cout<<i<<" "<<j<<" "<<dist[i][j]<<"\n";
-            q.pop();
-            if(j+1<m){
-             if(grid[i][j]==1){
-                 if(dist[i][j]<dist[i][j+1]){
-                     dist[i][j+1]=dist[i][j];
-                     q.push({i,j+1});
-                 }
-             }   
-             else{
-                 if(dist[i][j]+1<dist[i][j+1]){
-                     dist[i][j+1]=dist[i][j]+1;
-                     q.push({i,j+1});
-                 }
-                 
-             }
+int minCost(vector<vector<int>> &grid)
+{
+
+    int dirx[4] = {1, 0, -1, 0};
+    int diry[4] = {0, 1, 0, -1};
+
+    int m = grid.size();
+    int n = grid[0].size();
+    int max = 1e9;
+
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    int dist[m][n];
+    for (int i = 0; i < m; i++) {
+
+        for (int j = 0; j < n; j++) {
+            dist[i][j] = max;
+        }
+    }
+    dist[0][0] = 0;
+    while (!q.empty()) {
+
+        pair<int, int> top = q.front();
+
+        q.pop();
+
+        int x = top.first;
+        int y = top.second;
+
+        if (isValid(x + 1, y, m, n)) {
+
+            if (grid[x][y] == 3) {
+                if (dist[x + 1][y] > dist[x][y]) {
+                    dist[x + 1][y] = dist[x][y];
+                    q.push({x + 1, y});
+                }
             }
-            if(j-1>=0){
-                 if(grid[i][j]==2){
-                 if(dist[i][j]<dist[i][j-1]){
-                     dist[i][j-1]=dist[i][j];
-                     q.push({i,j-1});
-                 }
-             }   
-             else{
-                 if(dist[i][j]+1<dist[i][j-1]){
-                     dist[i][j-1]=dist[i][j]+1;
-                     q.push({i,j-1});
-                 }
-                 
-             }
-            }
-            if(i+1<n){
-                 if(grid[i][j]==3){
-                 if(dist[i][j]<dist[i+1][j]){
-                     dist[i+1][j]=dist[i][j];
-                     q.push({i+1,j});
-                 }
-             }   
-             else{
-                 if(dist[i][j]+1<dist[i+1][j]){
-                     dist[i+1][j]=dist[i][j]+1;
-                     q.push({i+1,j});
-                 }
-                 
-             }
-            }
-            if(i-1>=0){
-                 if(grid[i][j]==4){
-                 if(dist[i][j]<dist[i-1][j]){
-                     dist[i-1][j]=dist[i][j];
-                     q.push({i-1,j});
-                 }
-             }   
-             else{
-                 if(dist[i][j]+1<dist[i-1][j]){
-                     dist[i-1][j]=dist[i][j]+1;
-                     q.push({i-1,j});
-                 }
-                 
-             }
+            else {
+                if (dist[x + 1][y] > dist[x][y] + 1) {
+                    dist[x + 1][y] = dist[x][y] + 1;
+                    q.push({x + 1, y});
+                }
             }
         }
-        return dist[n-1][m-1];
+
+        if (isValid(x - 1, y, m, n)) {
+
+            if (grid[x][y] == 4) {
+                if (dist[x - 1][y] > dist[x][y]) {
+                    dist[x - 1][y] = dist[x][y];
+                    q.push({x - 1, y});
+                }
+            }
+            else {
+                if (dist[x - 1][y] > dist[x][y] + 1) {
+                    dist[x - 1][y] = dist[x][y] + 1;
+                    q.push({x - 1, y});
+                }
+            }
+        }
+
+        if (isValid(x, y + 1, m, n)) {
+
+            if (grid[x][y] == 1) {
+                if (dist[x][y + 1] > dist[x][y]) {
+                    dist[x][y + 1] = dist[x][y];
+                    q.push({x, y + 1});
+                }
+            }
+            else {
+                if (dist[x][y + 1] > dist[x][y] + 1) {
+                    dist[x][y + 1] = dist[x][y] + 1;
+                    q.push({x, y + 1});
+                }
+            }
+        }
+
+        if (isValid(x, y - 1, m, n)) {
+
+            if (grid[x][y] == 2) {
+                if (dist[x][y - 1] > dist[x][y]) {
+                    dist[x][y - 1] = dist[x][y];
+                    q.push({x, y - 1});
+                }
+            }
+            else {
+                if (dist[x][y - 1] > dist[x][y] + 1) {
+                    dist[x][y - 1] = dist[x][y] + 1;
+                    q.push({x, y - 1});
+                }
+            }
+        }
     }
+
+    return dist[m - 1][n - 1];
+}
 };

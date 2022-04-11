28ae7d5769953ee6363ff6481a6339ac7f836c8c
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-12 04:46:44 +0530

    Time: 888 ms (50.00%), Space: 165.5 MB (28.80%) - LeetHub

--- 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.cpp
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.cpp
@@ -1,61 +1,74 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+    int MOD=1e9+7;
+	
+	
+	// Step 3.
+    // to calculate the required output using dfs and memorization
+    // storing the number of paths from a node (index of mem) that leads to the nth node with ntoxSD in decreasing order in vector mem
+    int computeRequiredAnswer(int cnode, int n, vector<int> &mem, vector<int> &dist, int &ans, vector<vector<pair<int, int>>> &graph) {
+		// if we have reached the node n
+        if(cnode==n-1)
+            return 1;
         
-        vector<vector<pair<int,int>>>mp(n);
-        for(auto x:edges){
-            mp[x[0]-1].push_back({x[1]-1,x[2]});
-            mp[x[1]-1].push_back({x[0]-1,x[2]});
+		// if we have already computed for the current node
+        if(mem[cnode]!=-1)
+            return mem[cnode];
+        
+		// to compute mem[current node]
+        int cval=0;
+        for(auto a : graph[cnode]) {
+            if(dist[a.first]<dist[cnode]) {
+                cval=(cval+computeRequiredAnswer(a.first, n, mem, dist, ans, graph))%MOD;
+            }
         }
+        mem[cnode]=cval;
+        return cval;
+    }
+    
+	
+	// Step 2.
+    // to calculate shortest distance of all the nodes from nth node
+    vector<int> computeShortestPath(int n, vector<vector<pair<int, int>>> &graph) {
         vector<int> dist(n, INT_MAX);
         dist[n-1]=0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-        q.push({0,n-1});
-        while(!q.empty()){
-            int node=q.top().second;
-            int cost=q.top().first;
-            q.pop();
-            for(auto x:mp[node]){
-                if(x.second+cost<dist[x.first]){
-                    dist[x.first]=x.second+cost;
-                    q.push({dist[x.first],x.first});
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, n-1});
+        
+		//Dijkstra's Algorithm
+        while(!pq.empty()) {
+            int cnode=pq.top().second;
+            int cdist=pq.top().first;
+            pq.pop();
+            for(auto a : graph[cnode]) {
+                if(dist[a.first] > cdist+a.second) {
+                    dist[a.first]=cdist+a.second;
+                    pq.push({dist[a.first], a.first});
                 }
             }
         }
-        vector<int>mem(n,-1);
-        return dfs(mp,n,dist,0,mem);
-       
+        return dist;
     }
-    int dfs(vector<vector<pair<int,int>>>&mp,int n,vector<int>&dist,int curr,vector<int>&mem){
-        if(curr==n-1){
-            return 1;
+    
+	
+	// Step 1.
+    // main function
+    int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int, int>>> graph(n);
+        for(auto e : edges) {
+            graph[e[0]-1].push_back({e[1]-1, e[2]});
+            graph[e[1]-1].push_back({e[0]-1, e[2]});
         }
-        if(mem[curr]!=-1){
-            return mem[curr];
-        }
-        int ans=0;
-        for(auto x:mp[curr]){
-            if(dist[x.first]<dist[curr]){
-             ans=(ans%mod+dfs(mp,n,dist,x.first,mem)%mod)%mod;
-             
-            }
-        }
-        return mem[curr]=ans%mod;
+        
+		// to store the required shortest path
+        vector<int> dist;
+        dist=computeShortestPath(n, graph);
+		
+		// to store the number of paths that follows the given condition for each node
+        vector<int> mem(n, -1);
+        int ans=computeRequiredAnswer(0, n, mem, dist, ans, graph);
+		
+        return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

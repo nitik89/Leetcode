162e172f5c4ff607c9a0520a16b5777d8a51d1d9
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-16 21:35:07 +0530

    Time: 658 ms (100.00%), Space: 108.1 MB (66.67%) - LeetHub

---
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.cpp
@@ -0,0 +1,48 @@
+class ATM {
+public:
+    unordered_map<int,long long>mp;
+    vector<int>vec{20,50,100,200,500};
+    priority_queue<pair<int,int>>pq;
+    ATM() {
+       
+    }
+    
+    void deposit(vector<int> notes) {
+       for(int i=0;i<notes.size();i++){
+           mp[vec[i]]+=notes[i];
+       }
+    
+        
+    }
+    
+    vector<int> withdraw(int amount) {
+        
+        vector<int>ans(5,0);
+        for(int i=vec.size()-1;i>=0;i--){
+            long long mxnotes=min((long long)mp[vec[i]],(long long)amount/vec[i]);
+            if(mp[vec[i]]>=mxnotes&&mp[vec[i]]>0){
+                mp[vec[i]]-=mxnotes;
+                amount-=vec[i]*mxnotes;
+                ans[i]+=mxnotes;
+               
+            }
+           
+             
+        }
+        // cout<<amount<<" ";
+        if(amount>0){
+            for(int i=vec.size()-1;i>=0;i--){
+                mp[vec[i]]+=ans[i];
+            }
+            return {-1};
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your ATM object will be instantiated and called as such:
+ * ATM* obj = new ATM();
+ * obj->deposit(banknotesCount);
+ * vector<int> param_2 = obj->withdraw(amount);
+ */

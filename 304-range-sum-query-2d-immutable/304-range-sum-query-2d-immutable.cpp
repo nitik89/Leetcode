432e13d39f5fdfc193b88f97ef370126057e5e9e
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-19 23:50:01 +0530

    Time: 485 ms (73.90%), Space: 148 MB (50.08%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -3,45 +3,22 @@ public:
     vector<vector<int>>vec;
     NumMatrix(vector<vector<int>>& matrix) {
         int n=matrix.size(),m=matrix[0].size();
-        vec.resize(n + 1, vector<int>(m + 1));
+        vec.resize(n + 1, vector<int>(m + 1,0));
         
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                vec[i][j]=matrix[i][j];
+                vec[i+1][j+1]=vec[i+1][j]+matrix[i][j]+vec[i][j+1]-vec[i][j];
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=1;j<m;j++){
-                vec[i][j]+=vec[i][j-1];
-            }
-        }
-       
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){
-                vec[i][j]+=vec[i-1][j];
-            }
-        }
+        
          
         
         
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        if(col1-1>=0){
-            sum-=vec[row2][col1-1];
-           
-        }
-        if(row1-1>=0){
-            sum-=vec[row1-1][col2];
-             // cout<<vec[row1-1][col2]<<" ";
-        }
-        if(col1-1>=0&&row1-1>=0){
-            sum+=vec[row1-1][col1-1];
-        }
-        sum+=vec[row2][col2];
-        return sum;
+         return vec[row2 + 1][col2 + 1] - vec[row1][col2 + 1] - vec[row2 + 1][col1] + vec[row1][col1];
     }
 };
 

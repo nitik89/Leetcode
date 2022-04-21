5d26f110c305ed12126c7f9e5364cc0c1f22b175
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-21 14:03:51 +0530

    Time: 151 ms (48.39%), Space: 40.2 MB (83.48%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,19 +1,70 @@
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int v,Node* nxt){
+        val=v;
+        next=nxt;
+    }
+};
 class MyHashSet {
 public:
-    bool vis[1000001];
+    vector<Node*>vec;
+    int sz=101;
     MyHashSet() {
-        memset(vis,false,sizeof(vis));
+        vec.resize(101,NULL);
+    }
+    int getIdx(int k){
+        return k%sz;
     }
     
     void add(int key) {
-        vis[key]=1;
+        if(!contains(key)){
+        int idx=getIdx(key);
+         //we got a linked list here
+        Node* my=new Node(key,vec[idx]);
+        vec[idx]=my;
+        }
     }
     
     void remove(int key) {
-        vis[key]=0;
+        int idx=getIdx(key);
+        Node* temp=vec[idx];
+        Node* prev=NULL;
+        int fl=0;
+        while(temp!=NULL){
+            if(temp->val==key){
+                fl=1;
+                break;
+            }
+            prev=temp;
+            temp=temp->next;
+        }
+        if(prev==NULL&&fl==1){
+            vec[idx]=temp->next;
+        }
+        else if(fl==1){
+            prev->next=temp->next;
+        }
     }
     
     bool contains(int key) {
-        return vis[key];
+        int idx=getIdx(key);
+        Node* t=vec[idx];
+        while(t!=NULL){
+            if(t->val==key){
+                return true;
+            }
+            t=t->next;
+        }
+        return false;
     }
 };
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */

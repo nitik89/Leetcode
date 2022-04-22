f075a6ecf6fe717af4fa3be32b40dad820b6a1c8
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-22 16:52:34 +0530

    Time: 96 ms (99.50%), Space: 51.4 MB (90.71%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -0,0 +1,87 @@
+class Node{
+    public:
+    int key,val;
+    Node* next;
+    
+    Node(int k,int v,Node* nx){
+        key=k;
+        val=v;
+        next=nx;
+    }
+};
+class MyHashMap {
+public:
+    int sz=1001;
+    vector<Node*>vec;
+    MyHashMap() {
+        vec.resize(1001,NULL);
+    }
+    int getIndex(int idx){
+        return idx%sz;
+    }
+    Node* contains(int idx,int k){
+        Node* t=vec[idx];
+        while(t!=NULL){
+            if(t->key==k){
+                return t;
+            }
+            t=t->next;
+        }
+        return NULL;
+    }
+    
+    void put(int key, int value) {
+        int i=getIndex(key);
+        Node* yes=contains(i,key);
+        if(yes!=NULL){
+            yes->val=value;
+        }
+        else{
+            Node* node=new Node(key,value,vec[i]);
+            vec[i]=node;
+        }
+        
+    }
+    
+    int get(int key) {
+        int i=getIndex(key);
+        Node* node=contains(i,key);
+        if(node==NULL){
+            return -1;
+        }
+        return node->val;
+        
+    }
+    
+    void remove(int key) {
+        int i=getIndex(key);
+        Node* t=vec[i];
+        if(t==NULL){
+            return;
+        }
+        Node* prev=NULL;
+        if(t->key==key){
+            vec[i]=t->next;
+            return;
+        }
+        while(t!=NULL){
+            if(t->key==key){
+                prev->next=t->next;
+                break;
+            }
+            prev=t;
+            t=t->next;
+        }
+        if(prev==NULL){
+            vec[i]=t->next;
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */

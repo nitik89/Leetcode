07c3a831b6dc0bb7506ab80d1c279197f64edc26
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-23 07:35:40 +0530

    Time: 159 ms (51.62%), Space: 14.1 MB (33.82%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     
-    map<pair<int,int>,int>mp;
+    int dp[1<<17-1][17];
     bool getAns(int finalbit,int currbit,int csum,vector<int>&nums,int k,int target,int count){
        if(currbit==finalbit){
           
            return count==k;
        }
-        if(mp.find({currbit,count})!=mp.end()){
-            return mp[{currbit,count}];
+        if(dp[currbit][count]!=-1){
+            return dp[currbit][count];
         }
         for(int i=0;i<nums.size();i++){
             if(nums[i]+csum==target&&(((1<<i)&currbit)!=(1<<i))){
                if(getAns(finalbit,currbit|(1<<i),0,nums,k,target,count+1)){
-                   return mp[{currbit,count}]=true;
+                   return dp[currbit][count]=true;
                }
             }
             else if(nums[i]+csum<target&&(((1<<i)&currbit)!=(1<<i))){
                  if(getAns(finalbit,currbit|(1<<i),csum+nums[i],nums,k,target,count)){
-                     return mp[{currbit,count}]=true;
+                     return dp[currbit][count]=true;
                  }
             }
         }
-        return mp[{currbit,count}]=false;
+        return dp[currbit][count]=false;
        
     }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
@@ -32,11 +32,12 @@ public:
             sum+=x;
         }
         int finalbit=(1<<n)-1;
-       
+      
         int target=sum/k;
         if(sum%k!=0){
             return false;
         }
+        memset(dp,-1,sizeof dp);
         return getAns(finalbit,0,0,nums,k,target,0);
     }
 };

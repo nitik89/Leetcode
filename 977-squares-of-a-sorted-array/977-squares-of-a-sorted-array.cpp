4f0e54348ad7d730082f351be56becc7ed2a0d07
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-04-28 19:50:28 +0530

    Time: 42 ms (59.42%), Space: 27 MB (16.48%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -8,36 +8,30 @@ public:
                 break;
             }
         }
-        vector<int>pos;
-        vector<int>neg;
+       
         if(j==-1){
             j=nums.size();
         }
-        for(int i=j-1;i>=0;i--){
-            neg.push_back(nums[i]*nums[i]);
-        }
-        for(int i=j;i<nums.size();i++){
-            pos.push_back(nums[i]*nums[i]);
-        }
-        int i=0;
-        j=0;
+        
+        int i=j-1;
+        int n=j;
         vector<int>ans;
-        while(i<pos.size()&&j<neg.size()){
-            if(pos[i]>neg[j]){
-                ans.push_back(neg[j]);
+        while(i>=0&&j<nums.size()){
+            if(abs(nums[i])>abs(nums[j])){
+                ans.push_back(abs(nums[j]*nums[j]));
                 j++;
             }
             else{
-                ans.push_back(pos[i]);
-                i++;
+                ans.push_back(abs(nums[i]*nums[i]));
+                i--;
             }
         }
-        while(i<pos.size()){
-           ans.push_back(pos[i]);
-            i++;
+        while(i>=0){
+           ans.push_back(nums[i]*nums[i]);
+            i--;
         }
-         while(j<neg.size()){
-           ans.push_back(neg[j]);
+         while(j<nums.size()){
+           ans.push_back(nums[j]*nums[j]);
             j++;
         }
         return ans;

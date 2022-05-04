44c4113eee989a665af5cdb22b70fbde65f19cdb
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-05-05 02:40:19 +0530

    Time: 499 ms (32.79%), Space: 128.4 MB (59.98%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,38 +1,26 @@
 class TimeMap {
+    unordered_map<string, vector<pair<string, int>>> umap;
 public:
-    unordered_map<string,vector<pair<string,int>>>mp;
     TimeMap() {
         
     }
     
-    void set(string key, string value, int timestamp) {
-        mp[key].push_back({value,timestamp});
+    void set(string key, string value, int timestamp) {        
+        umap[key].push_back(make_pair(value, timestamp));
     }
     
     string get(string key, int timestamp) {
-        // vector<pair<string,int>>vec=mp[key];
-        int s=0,e=mp[key].size()-1;
-        string ans="";
-        while(s<=e){
-            int mid=(s+e)>>1;
-            if(mp[key][mid].second==timestamp){
-                return mp[key][mid].first;
-            }
-            if(mp[key][mid].second<timestamp){
-                ans=mp[key][mid].first;
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
+        int start = 0, end = umap[key].size() - 1;
+        string ans = "";
+        while(start<=end){
+            int mid = start + (end - start) / 2;
+            if(umap[key][mid].second == timestamp) return umap[key][mid].first;
+            else if(umap[key][mid].second < timestamp) {
+                ans = umap[key][mid].first;
+                start = mid + 1;  
+            } 
+            else end = mid-1;
         }
         return ans;
     }
 };
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap* obj = new TimeMap();
- * obj->set(key,value,timestamp);
- * string param_2 = obj->get(key,timestamp);
- */

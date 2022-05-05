74426724ce29a33e397b10672cab093b01aafb70
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-05-05 20:01:15 +0530

    Time: 11 ms (8.76%), Space: 7.1 MB (8.14%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,71 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int search(int i,int j,int tar,MountainArray &mount){
+        while(i<=j){
+            int mid=(i+j)>>1;
+            if(mount.get(mid)==tar){
+                return mid;
+            }
+            else if(mount.get(mid)>tar){
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
+        }
+        return -1;
+    }
+    int search1(int i,int j,int tar,MountainArray &mount){
+        while(i<=j){
+            int mid=(i+j)>>1;
+            if(mount.get(mid)==tar){
+                return mid;
+            }
+            else if(mount.get(mid)>tar){
+                i=mid+1;
+            }
+            else{
+                j=mid-1;
+            }
+        }
+        return -1;
+    }
+    int findInMountainArray(int target, MountainArray &mount) {
+        int i=0,j=mount.length()-1;
+        // cout<<mount.length()<<" ";
+        int idx=-1;
+        while(i<=j){
+            int mid=(i+j)>>1;
+            
+            if(i+1<mount.length()&&mount.get(mid)>mount.get(mid+1)){
+                idx=mid;
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
+        }
+         // cout<<idx<<"\n";
+        // return 1;
+        i=0;
+        j=mount.length()-1;
+    
+        int ans=search(i,idx,target,mount);
+        if(ans!=-1){
+            // cout<<ans<<"\n";
+            return ans;
+        }
+        
+        return search1(idx+1,j,target,mount);
+    }
+};

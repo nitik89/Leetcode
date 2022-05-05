565ec2d998c1b75b813319b198a78f7d2aac6681
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-05-05 20:03:11 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (8.14%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -1,71 +1,52 @@
-/**
- * // This is the MountainArray's API interface.
- * // You should not implement it, or speculate about its implementation
- * class MountainArray {
- *   public:
- *     int get(int index);
- *     int length();
- * };
- */
-
 class Solution {
 public:
-    int search(int i,int j,int tar,MountainArray &mount){
-        while(i<=j){
-            int mid=(i+j)>>1;
-            if(mount.get(mid)==tar){
+    int peakIndex(MountainArray &mountainArr,int lo, int hi)
+    {
+        while(lo<=hi)
+        {
+            int mid=lo+(hi-lo)/2;
+            if(mountainArr.get(mid)<mountainArr.get(mid+1))
+                lo=mid+1;
+            else
+                hi=mid-1;
+        }
+        return lo;
+    }
+    int bs1(MountainArray &mountainArr, int lo, int hi, int target)
+    {
+        while(lo<=hi)
+        {
+            int mid=lo+(hi-lo)/2;
+            if(mountainArr.get(mid)==target)
                 return mid;
-            }
-            else if(mount.get(mid)>tar){
-                j=mid-1;
-            }
-            else{
-                i=mid+1;
-            }
+            else if(mountainArr.get(mid)<target)
+                lo=mid+1;
+            else
+                hi=mid-1;
         }
         return -1;
     }
-    int search1(int i,int j,int tar,MountainArray &mount){
-        while(i<=j){
-            int mid=(i+j)>>1;
-            if(mount.get(mid)==tar){
+    int bs2(MountainArray &mountainArr, int lo, int hi, int target)
+    {
+        while(lo<=hi)
+        {
+            int mid=lo+(hi-lo)/2;
+            if(mountainArr.get(mid)==target)
                 return mid;
-            }
-            else if(mount.get(mid)>tar){
-                i=mid+1;
-            }
-            else{
-                j=mid-1;
-            }
+            else if(mountainArr.get(mid)>target)
+                lo=mid+1;
+            else
+                hi=mid-1;
         }
         return -1;
     }
-    int findInMountainArray(int target, MountainArray &mount) {
-        int i=0,j=mount.length()-1;
-        // cout<<mount.length()<<" ";
-        int idx=-1;
-        while(i<=j){
-            int mid=(i+j)>>1;
-            
-            if(i+1<mount.length()&&mount.get(mid)>mount.get(mid+1)){
-                idx=mid;
-                j=mid-1;
-            }
-            else{
-                i=mid+1;
-            }
-        }
-         // cout<<idx<<"\n";
-        // return 1;
-        i=0;
-        j=mount.length()-1;
-    
-        int ans=search(i,idx,target,mount);
-        if(ans!=-1){
-            // cout<<ans<<"\n";
-            return ans;
-        }
-        
-        return search1(idx+1,j,target,mount);
+    int findInMountainArray(int target, MountainArray &mountainArr) 
+    {
+        int n=mountainArr.length();
+        int peak=peakIndex(mountainArr,0,n-1);
+        cout<<peak;
+        int idx1=bs1(mountainArr,0,peak,target);
+        int idx2=bs2(mountainArr,peak,n-1,target);
+        return idx1==-1?idx2:idx1;
     }
 };

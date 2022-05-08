bf9a33af2e3c259cecdf162d37e178b336d6b517
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-05-08 16:14:23 +0530

    Time: 752 ms (73.86%), Space: 151.1 MB (60.68%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        
-        //hashing and prefix sum 
-        //calculate prefix sum -> in O(N4) -> calculate sum of all submatrices
-        //if sum==target count++;
         int row=matrix.size(),col=matrix[0].size();
-        
+        //generating the prefix sum type matrix
         for(int i=0;i<row;i++)
         {
             for(int j=1;j<col;j++) matrix[i][j]+=matrix[i][j-1];
@@ -15,29 +11,27 @@ public:
         {
             for(int i=1;i<row;i++) matrix[i][j]+=matrix[i-1][j];
         }
-        
-        int count=0;
-        
-        for(int sti=0;sti<row;sti++)
-        {
-            for(int stj=0;stj<col;stj++)
-            {
-                for(int endi=sti;endi<row;endi++)
-                {
-                    for(int endj=stj;endj<col;endj++)
-                    {
-                        int sum=matrix[endi][endj];
-                        if(stj>0) sum-=matrix[endi][stj-1];
-                        if(sti>0) sum-=matrix[sti-1][endj];
-                        if(sti>0 && stj>0) sum+=matrix[sti-1][stj-1];
-                        
-                        if(sum==target) count++;
+       
+        int cnt=0;
+              
+           for(int i=0;i<col;i++){
+            vector<int>sum(row,0);
+               
+            for(int j=i;j<col;j++){
+                unordered_map<int,int>mp;
+                mp[0]++;
+                for(int k=0;k<row;k++){
+                    int sm=matrix[k][j]-(i-1>=0?matrix[k][i-1]:0);
+                    // cout<<sm<<" ";
+                    if(mp.find(sm-target)!=mp.end()){
+                        cnt+=mp[sm-target];
                     }
+                    mp[sm]++;
                 }
+                
+                
             }
-        }
-        
-        return count;
-        
+           }
+        return cnt;
     }
 };

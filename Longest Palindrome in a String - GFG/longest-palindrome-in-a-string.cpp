86da0979e879d28c0481e8ad499152a40294562b
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-05-13 03:45:49 +0530

    Added solution - LeetHub

--- Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
@@ -18,21 +18,24 @@ class Solution {
                     dp[i][j]=1;
                 }
                 else if(gap==1){
-                    dp[i][j]=(s[i]==s[j])?2:0;
+                    dp[i][j]=(s[i]==s[j])?1:0;
                 }
                 else {
                 if(s[i]==s[j]){
                     if(dp[i+1][j-1]>0){
-                        dp[i][j]=dp[i+1][j-1]+2;
+                        dp[i][j]=dp[i+1][j-1];
                     }
                     else{
                         dp[i][j]=0;
                     }
                 }
+                else{
+                    dp[i][j]=0;
+                }
                     
                 }
-                if(mx<dp[i][j]){
-                mx=dp[i][j];
+                if(dp[i][j]&&mx<j-i+1){
+                mx=j-i+1;
                     idx=i;
                     vid=j;
                 }

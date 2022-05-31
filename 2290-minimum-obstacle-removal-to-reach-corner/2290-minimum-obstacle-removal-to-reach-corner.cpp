c1544d3acc001abf789323c0a012ff5b8bbbac5f
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-05-31 15:18:00 +0530

    Time: 1735 ms (17.12%), Space: 160.2 MB (27.66%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    int minimumObstacles(vector<vector<int>>& mat) {
-         vector<pair<int,int>>d={{-1,0},{0,1},{1,0},{0,-1}};
-        int n=mat.size();
-        int m=mat[0].size();
+    int minimumObstacles(vector<vector<int>>& grid) {
         queue<pair<int,int>>q;
+        int n=grid.size(),m=grid[0].size();
+        
+        vector<vector<int>>obs(n,vector<int>(m,INT_MAX));
         q.push({0,0});
-        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
-        dp[0][0]=0;
-        while(q.size())
-        {
-            int size=q.size();
-            for(int i=0;i<size;i++)
-            {
-                auto temp=q.front();
-                q.pop();
-                int x=temp.first;
-                int y=temp.second;
-                for(auto it:d)
-                {
-                    int nx=x+it.first;
-                    int ny=y+it.second;
-                    if(nx>=0&&ny>=0&&nx<n&&ny<m&&dp[x][y]+mat[nx][ny]<dp[nx][ny])
-                    {
-                        dp[nx][ny]=dp[x][y]+mat[nx][ny];
-                         q.push({nx,ny});
-                    }
+        vector<vector<int>>dirs{{-1,0},{0,1},{1,0},{0,-1}};
+        obs[0][0]=0;
+        int ans=INT_MAX;
+        while(!q.empty()){
+            int sz=q.size();
+            for(int p=0;p<sz;p++){
+            auto vec=q.front();
+           
+            int i=vec.first;
+            int j=vec.second;
+            
+            
+            q.pop();
+            
+            for(int k=0;k<dirs.size();k++){
+                int nx=i+dirs[k][0];
+                int ny=j+dirs[k][1];
+                if(nx>=n||ny>=m||nx<0||ny<0){
+                    continue;
+                }
+              
+                 if(obs[i][j]+grid[nx][ny]<obs[nx][ny]){
+                     obs[nx][ny]=obs[i][j]+grid[nx][ny];
+                    q.push({nx,ny});
                 }
             }
+            }
         }
-        return dp[n-1][m-1];
-                
+        return obs[n-1][m-1];
     }
 };

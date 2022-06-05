1e3890e1646260fb1d0457f6d13879c48b9f6572
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-06-05 23:22:02 +0530

    Time: 1042 ms (11.11%), Space: 266.8 MB (33.33%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.cpp
+++ 2296-design-a-text-editor/2296-design-a-text-editor.cpp
@@ -94,17 +94,8 @@ public:
             
             
         }
-        \
+        
         reverse(ans.begin(),ans.end());
         return ans;
     }
 };
-
-/**
- * Your TextEditor object will be instantiated and called as such:
- * TextEditor* obj = new TextEditor();
- * obj->addText(text);
- * int param_2 = obj->deleteText(k);
- * string param_3 = obj->cursorLeft(k);
- * string param_4 = obj->cursorRight(k);
- */

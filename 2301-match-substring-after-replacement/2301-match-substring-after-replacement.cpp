f8dac03e5f8a8abba31d5d82f5f8edde75958d30
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-06-12 16:57:18 +0530

    Time: 1914 ms (20.00%), Space: 17.9 MB (40.00%) - LeetHub

--- 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
- 
-    bool matchReplacement(string s, string sub, vector<vector<char>>& mapp) {
-        int len=sub.size();
-        unordered_map<char,unordered_set<char>>mp;
-        for(auto x:mapp){
-            mp[x[0]].insert(x[1]);
+    bool matchReplacement(string s, string sub, vector<vector<char>>& m) {
+        int n = s.length(), k = sub.length();
+        unordered_map<char,unordered_set<char>> mp;
+        for(int i=0;i<m.size();i++){
+            mp[m[i][0]].insert(m[i][1]);
         }
-        for(int i=0;i<=s.size()-len;i++){
-            string str=s.substr(i,len);
-            int f=0;
-           for(int j=0;j<sub.size();j++){
-               if(str[j]!=sub[j]){
-                   if(mp.find(sub[j])!=mp.end()){
-                   if(mp[sub[j]].count(str[j])==0){
-                       f=1;
-                       break;
-                   }
-                   }
-                   else{
-                       f=1;
-                       break;
-                   }
-               }
-              
-           }
-            if(f==0){
-                return true;
+
+        for(int i=0;i<=n-k;i++){
+            int flag = 0;
+            for(int j=0;j<k;j++){
+                char x = s[i+j], y = sub[j];
+                if(y==x || mp[y].find(x)!=mp[y].end()) continue;
+                else{
+                    flag = 1;
+                    break;
+                }
             }
+            if(flag==0) return true;
         }
         return false;
     }

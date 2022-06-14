5194247a044d949b796808f2adac5ef126c551b3
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-06-15 01:54:31 +0530

    Time: 1030 ms (9.40%), Space: 193.8 MB (6.57%) - LeetHub

---
+++ 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int>finalans;
+    int mx=INT_MIN;
+    void getAns(int arr,vector<int>&alice,int idx,int sum,vector<int>an){
+        if(arr<0){
+            return;
+        }
+        if(idx<0){
+            // cout<<arr<<" ";
+            if(mx<sum&&arr>=0){
+                
+                // cout<<sum<<" "<<arr<<"\n";
+                mx=sum;
+               
+                finalans=an;
+                finalans[0]+=arr;
+            }
+            return;
+        }
+        if(alice[idx]+1<=arr){
+            int curr=alice[idx]+1;
+            an.push_back(curr);
+            getAns(arr-(alice[idx]+1),alice,idx-1,sum+idx,an);
+            an.pop_back();
+        }
+        an.push_back(0);
+        getAns(arr,alice,idx-1,sum,an);
+        an.pop_back();
+    }
+    vector<int> maximumBobPoints(int arr, vector<int>& alice) {
+        getAns(arr,alice,alice.size()-1,0,{});
+        // vector<int>an;
+        reverse(finalans.begin(),finalans.end());
+         // cout<<mx<<" ";
+        return finalans;
+        
+    }
+};

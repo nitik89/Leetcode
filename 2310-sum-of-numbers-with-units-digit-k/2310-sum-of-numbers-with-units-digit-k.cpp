2d35a335dc9eb13df43a6de718bda80344a3f6ee
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-06-19 15:04:58 +0530

    Time: 7 ms (66.67%), Space: 5.9 MB (100.00%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
-    int cnt;
-    // int dp[3001][3001];
-    int getAns(int num,int k,int sum,int val,vector<vector<int>>&dp){
-        if(num<0||k>sum){
-            return 1e9;
-        }
-     if(num==0){
-         
-         return dp[num][k]=0;
-     }
-        if(dp[num][k]!=-1){
-            return dp[num][k];
-        }
-        int mx=1e9;
-        
-        
-        
-        for(int i=1;i*k<=num;i++){
-            mx=min(mx,getAns(num-i*k,k,sum,val,dp)+i);
-        }
-        mx=min(mx,getAns(num,k+10,sum,val,dp));
-        
-        
-        return dp[num][k]=mx;
-    }
+  
     int minimumNumbers(int num, int k) {
-        
         if(num==0){
             return 0;
         }
-        if(k==0&&num%10==0){
-            return 1;
+        for(int i=1;i<=10;i++){
+            int val=i*k;
+            if(val%10==num%10 && val<=num){
+                return i;
+            }
         }
-        if(k==0){
-            return -1;
-        }
-        vector<vector<int>>dp(num+1,vector<int>(num+1,-1));
-        
-        cnt=INT_MAX;
-        int ans=getAns(num,k,num,0,dp);
-        return ans==1e9?-1:ans;
-        
+        return -1;
     }
 };

6de17d2786df5dc2f5bdd5bd85024091ef052f9d
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-06-21 17:43:05 +0530

    Time: 318 ms (6.26%), Space: 21.9 MB (14.47%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,36 +1,40 @@
-class Solution {
-public:
-    vector<int> smallestRange(vector<vector<int>>& nums) {
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-        int mx=INT_MIN;
-     for(int i=0;i<nums.size();i++){
-         for(int j=0;j<1;j++){
-             // vec.push_back({i,j});
-             pq.push({nums[i][j],i,j});
-             mx=max(nums[i][j],mx);
-         }
-     }
-        
-        int range=INT_MAX;
-        int l=-1,r=-1;
-        while(true){
-           vector<int>vec= pq.top();
-            pq.pop();
-           if(mx-vec[0]<range){
-               range=(mx-vec[0]);
-               r=mx;
-               l=vec[0];
-           }
-           int i=vec[1],j=vec[2];
-            if(j+1<nums[i].size()){
-                pq.push({nums[i][j+1],i,j+1});
-                mx=max(nums[i][j+1],mx);
+class Solution
+{
+    public:
+        vector<int> smallestRange(vector<vector < int>> &nums)
+        {
+            priority_queue<vector < int>, vector< vector< int >>, greater<vector< int>>> pq;
+            int mini = INT_MAX, maxi = INT_MIN;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                pq.push({ nums[i][0],
+                    i,
+                    0 });
+                maxi = max(maxi, nums[i][0]);
             }
-            else{
-                break;
+            vector<int> ans(2);
+            ans[0] = -1e9;
+            ans[1] = 1e9;
+            while (true)
+            {
+                auto v = pq.top();
+                pq.pop();
+                mini = v[0];
+                int i = v[1], j = v[2];
+                if ((maxi - mini < ans[1] - ans[0]) || (maxi - mini == ans[1] - ans[0] && mini < ans[0]))
+                {
+                    ans[0] = mini;
+                    ans[1] = maxi;
+                }
+                if (j + 1 < nums[i].size())
+                {
+                    pq.push({ nums[i][j + 1],
+                        i,
+                        j + 1 });
+                    maxi = max(maxi, nums[i][j + 1]);
+                }
+                else break;
             }
+            return ans;
         }
-       
-        return {l,r};
-    }
 };

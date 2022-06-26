495ecc67008070d9563c85ee445ab262d2754921
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-06-26 10:40:32 +0530

    Time: 35 ms (100.00%), Space: 16.3 MB (33.33%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    if(grid[i][j]==0){
+                        return false;
+                    }
+                    
+                }
+                else if(i+j==(n-1)){
+                    // cout<<grid[i][j]<<" ";
+                    if(grid[i][j]==0){
+                        return false;
+                    }
+                }
+                else{
+                    if(grid[i][j]!=0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};

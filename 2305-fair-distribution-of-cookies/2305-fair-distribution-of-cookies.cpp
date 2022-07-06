f12b3915f48b51294327656749c60ece321ab737
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-07-07 03:15:13 +0530

    Time: 1413 ms (20.66%), Space: 7 MB (13.42%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int mx=INT_MAX;
-    void getAns(vector<int>&cookies,int n,vector<int>&space,int j){
+    void getAns(vector<int>&cookies,int n,vector<int>&space,int j,int mx1){
         
         if(j==n){
-            int mx1=INT_MIN;
-            for(auto x:space){
-                mx1=max(x,mx1);
-            }
+           
+            
             mx=min(mx1,mx);
             return;
         }
         for(int i=0;i<space.size();i++){
             space[i]+=cookies[j];
-            getAns(cookies,n,space,j+1);
+            getAns(cookies,n,space,j+1,max(mx1,space[i]));
             space[i]-=cookies[j];
         }
         
@@ -21,7 +19,7 @@ public:
     int distributeCookies(vector<int>& cookies, int k) {
         int n=cookies.size();
         vector<int>space(k,0);
-        getAns(cookies,n,space,0);
+        getAns(cookies,n,space,0,INT_MIN);
         return mx;
     }
 };

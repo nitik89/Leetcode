739c84fb4dd99062023b63e1f8c286513d1b152c
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-08-07 20:36:15 +0530

    Time: 92 ms (10.88%), Space: 6 MB (65.28%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        int v=pow(2,29);
-        vector<pair<int,int>>vec={{},{0,3},{4,6},{7,9},{10,13},{14,16},{17,19},{20,23},{24,26},{27,29}};
-        int dig=floor(log10(n) + 1);
-        int num=n;
-        unordered_map<int,int>mp;
-        while(num>0){
-            int v=num%10;
-            mp[v]++;
-            num=num/10;
-        }
-        int f=vec[dig].first;
-        int l=vec[dig].second;
-        for(int i=f;i<=l;i++){
-            unordered_map<int,int>mp1;
-           int v=pow(2,i);
-            while(v>0){
-            int k=v%10;
-            mp1[k]++;
-            v=v/10;
-        }
-            int k=0;
-            for(auto x:mp1){
-                if(mp[x.first]!=x.second){
-                    k=1;
-                    break;
-                }
+        string  s = to_string(n);
+        sort(s.begin() , s.end());
+        do{
+            int x = stoi(s);
+            int ct = 0;
+            while(x!=0){
+                x = x&(x-1);
+                ct++;
             }
-            if(!k){
+            if(ct==1 and s[0]!='0'){
                 return true;
             }
-            
-        }
+        }while(next_permutation(s.begin() , s.end()));
         return false;
-        
     }
 };

9cd5aa0609475ff15394751f311093c5db1a5b31
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-08-16 18:58:34 +0530

    Time: 344 ms (64.20%), Space: 9.2 MB (63.23%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int dp[1<<14+1][16];
+    int dp[(1<<14)+1][16];
     int getAns(vector<int>&tasks,int cs,int sum,int mask){
         // cout<<mask<<" "<<((1<<tasks.size())-1)<<"\n";
         if(mask==((1<<tasks.size())-1)){
-            return cs!=0;
+            return 0;
         }
-        if(cs>sum){
-            return 1e9;
-        }
+        
         if(dp[mask][cs]!=-1){
             return dp[mask][cs];
         }
         int mn=1e9;
         for(int i=0;i<tasks.size();i++){
             if((mask&(1<<i)))continue;
-            if(cs+tasks[i]==sum){
-              mn=min(mn, 1+getAns(tasks,0,sum,mask|(1<<i)));
+            if(cs+tasks[i]<=sum){
+               mn=min(mn,getAns(tasks,cs+tasks[i],sum,mask|(1<<i)));
             }
-            else if(cs+tasks[i]<sum){
-            mn=min(mn,getAns(tasks,cs+tasks[i],sum,mask|(1<<i)));
-            mn=min(mn,1+getAns(tasks,0,sum,mask|(1<<i)));
+            else{
+                mn=min(mn,1+getAns(tasks,tasks[i],sum,mask|(1<<i)));
             }
             
         }
@@ -28,6 +25,6 @@ public:
     }
     int minSessions(vector<int>& tasks, int st) {
         memset(dp,-1,sizeof dp);
-      return getAns(tasks,0,st,0);
+      return getAns(tasks,0,st,0)+1;
     }
 };

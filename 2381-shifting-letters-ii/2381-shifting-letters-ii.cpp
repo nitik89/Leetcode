d76d6bea6ea0693bb58e6725d4482508f7ab75cc
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-08-20 23:07:52 +0530

    Time: 404 ms (80.00%), Space: 103.9 MB (60.00%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>&sh) {
         vector<int>vec(s.size()+1,0);
+        string str="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+        
+        int i=0;
+        
+        
         for(auto x:sh){
             int st=x[0];
             int end=x[1];
@@ -20,13 +25,15 @@ public:
         for(int i=1;i<vec.size();i++){
             vec[i]+=vec[i-1];
         }
-        int i=0;
-        for(auto x:vec){
-            // cout<<(s[i] - 'a')<<" ";
-            s[i]=(((s[i] - 'a') + x%26+26)%26 + 'a');
+     
+        for(int i=0;i<vec.size()-1;i++){
+            // cout<<i<<" ";
+            int sh=vec[i]%26;
+            int idx=26+(s[i] - 'a');
+            // cout<<idx-sh<<" ";
+            // cout<<sh<<" "<<idx<<"\n";
+            s[i]=str[idx+sh];
             
-            
-            i++;
         }
         return s;
         

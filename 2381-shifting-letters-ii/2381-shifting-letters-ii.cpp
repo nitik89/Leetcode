ac17fcfe8fc568c6d146f372badbd28d27a933a9
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-08-20 22:41:18 +0530

    Time: 851 ms (20.00%), Space: 103.6 MB (60.00%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -22,34 +22,8 @@ public:
         }
         int i=0;
         for(auto x:vec){
-            
-            int v=x%26;
-             // cout<<v<<" ";
-            if(v>0){
-                int shift;
-                if(('z'- s[i] )>=abs(v)){
-                    shift=(v);
-                    s[i]=(char)(s[i] + shift);
-                }
-                else{
-                    int shift=(abs(v)-('z' - s[i])-1);
-                    s[i]=(char)('a' + shift);
-                }
-            }
-            else if(v<0){
-                int shift;
-                if((s[i] - 'a')>=abs(v)){
-                    shift=(v);
-                    s[i]=(char)(s[i] + shift);
-                }
-                else{
-                    shift=26-(abs(v)-(s[i] - 'a'));
-                    s[i]=(char)('a' + shift);
-                }
-                
-                
-                
-            }
+            // cout<<(s[i] - 'a')<<" ";
+            s[i]=(((s[i] - 'a') + x%26+26)%26 + 'a');
             
             
             i++;

5275d72b0a032daf8f5027588ba40d6e090774a1
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-08-22 10:00:57 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (6.02%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0){
+            return false;
+        }
+       float x=(log(n)/log(4));
+        if(ceil(x)==x){
+            return true;
+        }
+        return false;
+    }
+};

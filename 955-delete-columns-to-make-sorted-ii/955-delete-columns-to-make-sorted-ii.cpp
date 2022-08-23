bb6c117a42e6761d0ff4124cf7b3bbd60769ade5
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-08-24 02:05:54 +0530

    Time: 18 ms (33.20%), Space: 10.5 MB (26.14%) - LeetHub

--- 955-delete-columns-to-make-sorted-ii/955-delete-columns-to-make-sorted-ii.cpp
+++ 955-delete-columns-to-make-sorted-ii/955-delete-columns-to-make-sorted-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
         int n=strs.size();
-        vector<string>temp(n,"");
+        // vector<string>temp(n,"");
         vector<string>vec(n,"");
         int ans=strs[0].size();
         for(int j=0;j<strs[0].size();j++){
@@ -11,13 +11,13 @@ public:
             char str=strs[i][j];
             // cout<<str<<" ";
             vec[i].push_back(str);
-            temp[i].push_back(str);
+            // temp[i].push_back(str);
         }
-            sort(temp.begin(),temp.end());
+            // sort(temp.begin(),temp.end());
             int b=0;
-            for(int i=0;i<n;i++){
+            for(int i=1;i<n;i++){
                 // cout<<temp[i]<<" "<<vec[i]<<"\n";
-                if(temp[i]!=vec[i]){
+                if(vec[i]<vec[i-1]){
                     
                     b=1;
                     break;
@@ -25,7 +25,7 @@ public:
             }
             if(b){
                 for(int i=0;i<n;i++){
-                    temp[i].pop_back();
+                    // temp[i].pop_back();
                     vec[i].pop_back();
                 }
             }

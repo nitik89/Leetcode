9d8134248b625186ffc371a6e23402fdf4acb741
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-08-26 06:47:55 +0530

    Time: 6 ms (50.57%), Space: 6.6 MB (29.94%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        map<int,int>mp1;
-        
-        while(n)
-        {
-            mp1[n%10]++;
-            n=n/10;
+        int v=pow(2,29);
+        vector<pair<int,int>>vec={{},{0,3},{4,6},{7,9},{10,13},{14,16},{17,19},{20,23},{24,26},{27,29}};
+        int dig=floor(log10(n) + 1);
+        int num=n;
+        unordered_map<int,int>mp;
+        while(num>0){
+            int v=num%10;
+            mp[v]++;
+            num=num/10;
         }
-        for(int i=0;i<32;i++)
-        {
-            map<int,int>mp2;
-            
-            long long p=pow(2,i);
-            while(p)
-            {
-                mp2[p%10]++;
-                p=p/10;
-                
-                
+        int f=vec[dig].first;
+        int l=vec[dig].second;
+        for(int i=f;i<=l;i++){
+            unordered_map<int,int>mp1;
+           int v=pow(2,i);
+            while(v>0){
+            int k=v%10;
+            mp1[k]++;
+            v=v/10;
+        }
+            int k=0;
+            for(auto x:mp1){
+                if(mp[x.first]!=x.second){
+                    k=1;
+                    break;
+                }
             }
-            if(mp1==mp2)
-            {
+            if(!k){
                 return true;
             }
+            
         }
         return false;
+        
     }
 };

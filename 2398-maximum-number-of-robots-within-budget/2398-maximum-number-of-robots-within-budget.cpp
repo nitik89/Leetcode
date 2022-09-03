edd77283d2d0b6762ae104ad6a68870cbfe4e8fe
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-09-03 22:50:12 +0530

    Time: 657 ms (20.00%), Space: 137.8 MB (40.00%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumRobots(vector<int>& ct, vector<int>& rc, long long budget) {
+         set<long long, greater<long long> > st;
+        int s=0,e=0;
+        long long sum=0;
+        int ans=0;
+        int n=ct.size();
+        while(e<n){
+            sum+=rc[e];
+            st.insert(ct[e]);
+            while(s<e&&((*st.begin()+(long long)(e-s+1)*sum)>budget)){
+                sum-=rc[s];
+                st.erase(ct[s]);
+                s++;
+                
+            }
+            long long t=(*st.begin()+(e-s+1)*sum);
+            if(t<=budget){
+                ans=max(e-s+1,ans);
+            }
+            e++;
+            
+        }
+        return ans;
+    }
+};

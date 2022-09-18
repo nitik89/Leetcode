b7ff6ac15726e31823e76628e06b022fdae60b9c
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-09-18 11:13:35 +0530

    Time: 672 ms (62.50%), Space: 462 MB (12.50%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
@@ -1,26 +1,64 @@
+class Node{
+    public:
+    Node*child[26];
+    int cnt;
+    bool flag;
+    Node(){
+    for(int i=0;i<26;i++){
+        child[i]=NULL;
+    }
+        flag=false;
+        cnt=0;
+    }
+};
+class Trie{
+    public:
+    Node * root;
+    Trie(){
+        root=new Node();
+    }
+    void insert(string str){
+        Node* t=root;
+        for(int i=0;i<str.size()&&t!=NULL;i++){
+            if(t -> child[ str[i] - 'a' ]==NULL){
+                t -> child[ str[i] - 'a' ] = new Node();
+            }
+            t = t -> child[str[i] - 'a'];
+            t -> cnt++;
+        }
+        t -> flag = true;
+    }
+    int getCount(string str){
+        Node * t = root;
+        int sum=0;
+        for(int i=0;i<str.size();i++){
+            if(t -> child[str[i] - 'a'] == NULL){
+                return sum;
+            }
+            t = t->child[str[i] - 'a'];
+            sum+=t->cnt;
+        } 
+        return sum;
+    }
+};
 class Solution {
 public:
     vector<int> sumPrefixScores(vector<string>& words) {
-        const uint64_t MOD=184467440737091;
-     unordered_map<uint64_t,int>mp;
+       Trie *tr=new Trie();
         for(int i=0;i<words.size();i++){
-            uint64_t s=0;
-            for(int j=0;j<words[i].size();j++){
-                s=(131313*s+(words[i][j] - 96))%MOD;
-                mp[s]++;
-            }
+            tr->insert(words[i]);
         }
-        vector<int>ans(words.size(),0);
+        vector<int>ans;
         for(int i=0;i<words.size();i++){
-            uint64_t s=0;
-            int cnt=0;
-            for(int j=0;j<words[i].size();j++){
-                s = (131313 * s + (words[i][j] - 96)) % MOD;
-                cnt+=mp[s];
-            }
-            ans[i]+=cnt;
+            int v=tr->getCount(words[i]);
+            ans.push_back(v);
         }
         return ans;
-        
     }
 };
+
+
+
+
+
+

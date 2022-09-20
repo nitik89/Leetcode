775f622df08271fe2bddfa8da37e631b57bacdd8
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-09-20 18:53:32 +0530

    Time: 399 ms (48.73%), Space: 91.1 MB (84.15%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -38,36 +38,44 @@ public:
 //     }
     int minSwap(vector<int>& vec1, vector<int>& vec2) {
         memset(dp,0,sizeof dp);
-        
+        int a1=0,a2=0;
         for(int i=vec1.size()-2;i>=0;i--){
+            int t1=0,t2=0;
             for(int s=0;s<=1;s++){
                 int ans=INT_MAX;
             if(s){
                 if(vec2[i+1]>vec1[i]&&vec1[i+1]>vec2[i]){
-                    ans=min(ans,dp[i+1][0]);
+                    ans=min(ans,a1);
                 }
                 
                     
                 
                 if(vec1[i+1]>vec1[i]&&vec2[i+1]>vec2[i]){
-                ans=min(ans,1+dp[i+1][1]);
+                ans=min(ans,1+a2);
                 }
             }
             else{
                 if(vec1[i+1]>vec1[i]&&vec2[i+1]>vec2[i]){
-                    ans=min(ans,dp[i+1][0]);
+                    ans=min(ans,a1);
                 }
                 if(vec2[i+1]>vec1[i]&&vec1[i+1]>vec2[i]){
-                    ans=min(ans,1+dp[i+1][1]);
+                    ans=min(ans,1+a2);
                 }
                 }
-                dp[i][s]=ans;
+                if(s==0){
+                    t1=ans;
+                }
+                else{
+                    t2=ans;
+                }
             }
+            a1=t1;
+            a2=t2;
             
                 
                 
             }
-        return min(1+dp[0][1],dp[0][0]);
+        return min(1+a2,a1);
         }
     
 };

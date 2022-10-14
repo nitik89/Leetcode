3448244b9ba0b831c17bdc562177a7689c3aa8b5
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-10-14 08:02:16 +0530

    Time: 667 ms (38.14%), Space: 22.8 MB (43.46%) - LeetHub

---
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.cpp
@@ -0,0 +1,73 @@
+class Solution
+{
+    public:
+
+        vector<int> par;
+    vector<int> rank;
+    int find(int x)
+    {
+        if (par[x] == x)
+        {
+            return x;
+        }
+        int y = find(par[x]);
+        par[x] = y;
+        return y;
+    }
+    void _union(int a, int b)
+    {
+        int x = find(a);
+        int y = find(b);
+        if (x == y)
+        {
+            return;
+        }
+        if (rank[x] > rank[y])
+        {
+            par[y] = x;
+        }
+        else if (rank[y] > rank[x])
+        {
+            par[x] = y;
+        }
+        else
+        {
+            par[x] = y;
+            rank[y]++;
+        }
+    }
+    vector<bool> friendRequests(int n, vector<vector < int>> &res, vector< vector< int>> &req)
+    {
+        par.resize(n);
+        rank.resize(n);
+        for (int i = 0; i < n; i++)
+        {
+            par[i] = i;
+            rank[i] = 0;
+        }
+
+        vector<bool> ans;
+        for (auto a: req)
+        {
+            int x = find(a[0]);
+            int y = find(a[1]);
+            int r = 1;
+            for (auto &m: res)
+            {
+                int p = find(m[0]);
+                int q = find(m[1]);
+                if ((p == x && y == q) || (p == y && x == q))
+                {
+                    r = 0;
+                    break;
+                }
+            }
+            ans.push_back(r);
+            if (r)
+            {
+                _union(a[0], a[1]);
+            }
+        }
+        return ans;
+    }
+};

6d38b7bf11c2ce74e30beed968e2feda70cb3afa
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-10-15 22:24:32 +0530

    Time: 1029 ms (15.38%), Space: 160.9 MB (7.69%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -1,47 +1,51 @@
-class Solution {
-public:
-  int highestPowerof2(int n)
+class Solution
 {
-   int p = (int)log2(n);
-   return (int)pow(2, p);
-}
-    vector<int> productQueries(int n, vector<vector<int>>& q) {
-        
-        vector<long long>vec;
-        int mod=1e9+7;
-        long long s = highestPowerof2(n);
-        long long sum=0;
-        // cout<<s<<" ";
-        while(sum<=n&&s>=1){
-            // cout<<sum<<" ";
-            if(sum+s<=n){
-            vec.push_back(s);
-            sum+=s; 
+    public:
+        long long highestPowerof2(int n)
+        {
+            for (int i = 0; i < 31; i++)
+            {
+                long long x = pow(2, i);
+                if (x >= n)
+                {
+                    return x;
+                }
             }
-            s=s/2;
-            
+            return 1;
         }
-        cout<<sum<<" ";
-        reverse(vec.begin(),vec.end());
-       // for(int i=1;i<vec.size();i++){
-       //     vec[i]=((vec[i])*(vec[i-1]))%mod;
-       // }
-        // for(auto x:vec){
-        //     cout<<x<<" ";
-        // }
-        vector<int>ans;
-         for(auto x:q){
-            int s=x[0];
-            int e=x[1];
-            long long a=1;
-            for(int j=s;j<=e;j++){
-                a*=(vec[j])%mod;
-                a=a%mod;
+    vector<int> productQueries(int n, vector<vector < int>> &q)
+    {
+
+        vector < long long > vec;
+        int mod = 1e9 + 7;
+        long long s = highestPowerof2(n);
+        long long sum = 0;
+       	// cout<<s<<" ";
+        while (sum <= n && s >= 1)
+        {
+           	// cout<<sum<<" ";
+            if (sum + s <= n)
+            {
+                vec.push_back(s);
+                sum += s;
+            }
+            s = s / 2;
+        }
+       	// cout<<sum<<" ";
+        reverse(vec.begin(), vec.end());
+        vector<int> ans;
+        for (auto x: q)
+        {
+            int s = x[0];
+            int e = x[1];
+            long long a = 1;
+            for (int i = s; i <= e; i++)
+            {
+                a *= vec[i] % mod;
+                a = a % mod;
             }
             ans.push_back(a);
-            
         }
         return ans;
-        
     }
 };

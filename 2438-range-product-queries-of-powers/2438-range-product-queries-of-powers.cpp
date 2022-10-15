ffeb60051965b462a94857332371b04b21c39143
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-10-15 21:56:28 +0530

    Time: 1015 ms (15.38%), Space: 161 MB (7.69%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
-   int highestPowerof2(unsigned int n)
+  int highestPowerof2(int n)
 {
-    // Invalid input
-    if (n < 1)
-        return 0;
-    int res = 1;
-    // Try all powers starting from 2^1
-    for (int i = 0; i < 8 * sizeof(unsigned int); i++) {
-        int curr = 1 << i;
-        // If current power is more than n, break
-        if (curr > n)
-            break;
-        res = curr;
-    }
-    return res;
-
+   int p = (int)log2(n);
+   return (int)pow(2, p);
 }
     vector<int> productQueries(int n, vector<vector<int>>& q) {
         

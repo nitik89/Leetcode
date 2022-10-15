a0023e950e258d64b6373a446a7e4719503a3f52
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-10-15 21:53:59 +0530

    Time: 896 ms (15.38%), Space: 160.8 MB (7.69%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -1,36 +1,57 @@
 class Solution {
 public:
- 
-    vector<int> productQueries(int x, vector<vector<int>>& q) {
-        vector<long long>v;
+   int highestPowerof2(unsigned int n)
+{
+    // Invalid input
+    if (n < 1)
+        return 0;
+    int res = 1;
+    // Try all powers starting from 2^1
+    for (int i = 0; i < 8 * sizeof(unsigned int); i++) {
+        int curr = 1 << i;
+        // If current power is more than n, break
+        if (curr > n)
+            break;
+        res = curr;
+    }
+    return res;
+
+}
+    vector<int> productQueries(int n, vector<vector<int>>& q) {
+        
         vector<long long>vec;
         int mod=1e9+7;
-         while (x > 0) 
-    {
-        v.push_back(x % 2);
-        x = x / 2;
-    }
-  
-   
-    for (int i = 0; i < v.size(); i++) 
-    {
-        if (v[i] == 1) 
-        {
-            vec.push_back(pow(2,i));
+        long long s = highestPowerof2(n);
+        long long sum=0;
+        // cout<<s<<" ";
+        while(sum<=n&&s>=1){
+            // cout<<sum<<" ";
+            if(sum+s<=n){
+            vec.push_back(s);
+            sum+=s; 
+            }
+            s=s/2;
+            
         }
-    }
-        
-       
+        cout<<sum<<" ";
+        reverse(vec.begin(),vec.end());
+       // for(int i=1;i<vec.size();i++){
+       //     vec[i]=((vec[i])*(vec[i-1]))%mod;
+       // }
+        // for(auto x:vec){
+        //     cout<<x<<" ";
+        // }
         vector<int>ans;
-        for(auto &x:q){
+         for(auto x:q){
             int s=x[0];
             int e=x[1];
-            long long div=1;
+            long long a=1;
             for(int j=s;j<=e;j++){
-                div*=vec[j]%mod;
-                div=div%mod;
+                a*=(vec[j])%mod;
+                a=a%mod;
             }
-            ans.push_back(div);
+            ans.push_back(a);
+            
         }
         return ans;
         

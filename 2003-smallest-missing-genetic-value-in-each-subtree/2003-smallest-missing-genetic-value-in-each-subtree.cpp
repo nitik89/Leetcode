9bb46140a0427a912e0d1abd1f416708e3e9ec2f
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-10-19 04:14:45 +0530

    Time: 544 ms (76.37%), Space: 144.6 MB (69.78%) - LeetHub

--- 2003-smallest-missing-genetic-value-in-each-subtree/2003-smallest-missing-genetic-value-in-each-subtree.cpp
+++ 2003-smallest-missing-genetic-value-in-each-subtree/2003-smallest-missing-genetic-value-in-each-subtree.cpp
@@ -19,35 +19,36 @@ class Solution
     }
     vector<int> smallestMissingValueSubtree(vector<int> &pr, vector<int> &nums)
     {
-        int n=pr.size();
-        
+        int n = pr.size();
+
         vis.resize(n, 0);
         vector<vector < int>> vec(n);
-        
+
         for (int i = 1; i < pr.size(); i++)
         {
-            // cout<<pr[i]<<" ";
+           	
             vec[pr[i]].push_back(i);
         }
         int node = -1;
-       
+
         for (int i = 0; i < nums.size(); i++)
         {
             if (nums[i] == 1)
             {
-                node = i;
+                node = i;//searching only in the subtrees where 1 is present so we will apply dfs to find out which all the values are present in those subtrees and we will then find the minimum possible value out of them so we will keep on moving to its parents only
                 break;
             }
         }
-        int mx=INT_MIN;
-        for(auto x:nums){
-            mx=max(x,mx);
+        int mx = INT_MIN;
+        for (auto x: nums)
+        {
+            mx = max(x, mx);
         }
-         mark.resize(mx+1, 0);
-        
+        mark.resize(mx + 1, 0);
+
         int miss = 1;
         vector<int> ans(n, 1);
-        
+
         while (node != -1)
         {
             dfs(vec, node, nums);
@@ -55,7 +56,7 @@ class Solution
             {
                 miss++;
             }
-            // cout<<node<<" ";
+           	// cout<<node<<" ";
             ans[node] = miss;
             node = pr[node];
         }

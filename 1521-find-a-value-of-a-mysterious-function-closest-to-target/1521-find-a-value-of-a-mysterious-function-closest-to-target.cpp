c702fc87c54ee14b3203e09b497e3c99a07c8e9b
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-10-26 23:37:37 +0530

    Time: 2216 ms (16.30%), Space: 72.8 MB (54.82%) - LeetHub

--- 1521-find-a-value-of-a-mysterious-function-closest-to-target/1521-find-a-value-of-a-mysterious-function-closest-to-target.cpp
+++ 1521-find-a-value-of-a-mysterious-function-closest-to-target/1521-find-a-value-of-a-mysterious-function-closest-to-target.cpp
@@ -14,9 +14,9 @@ class Solution
             return;
         }
         int mid = (low + high) / 2;
-        if (index <= mid) update(low, mid, index, val, 2*pos + 1);
-        else update(mid + 1, high, index, val, 2*pos + 2);
-        tree[pos] = tree[2*pos + 1] & tree[2*pos + 2];
+        if (index <= mid) update(low, mid, index, val, 2 *pos + 1);
+        else update(mid + 1, high, index, val, 2 *pos + 2);
+        tree[pos] = tree[2 *pos + 1] &tree[2 *pos + 2];
     }
 
     int query(int qlow, int qhigh, int low, int high, int pos)
@@ -31,30 +31,36 @@ class Solution
         }
         int mid = (low + high) / 2;
        	// cout << pos << endl;
-        int s1 = query(qlow, qhigh, low, mid, 2*pos + 1);
-        int s2 = query(qlow, qhigh, mid + 1, high, 2*pos + 2);
-        return s1 & s2;
+        int s1 = query(qlow, qhigh, low, mid, 2 *pos + 1);
+        int s2 = query(qlow, qhigh, mid + 1, high, 2 *pos + 2);
+        return s1 &s2;
     }
-    int closestToTarget(vector<int> &arr, int target) {
-        
-        int n=arr.size();
-        tree.resize(4*n);
-        for(int i=0;i<n;i++){
-            update(0,n-1,i,arr[i],0);
+    int closestToTarget(vector<int> &arr, int target)
+    {
+
+        int n = arr.size();
+        tree.resize(4 *n);
+        for (int i = 0; i < n; i++)
+        {
+            update(0, n - 1, i, arr[i], 0);
         }
-        int ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            int left=i,right=n-1;
-            while(left<=right){
-                int mid=(left+right)/2;
-                int v=query(i,mid,0,n-1,0);
-                if(v<target){
-                    right=mid-1;
+        int ans = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            int left = i, right = n - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                int v = query(i, mid, 0, n - 1, 0);
+                if (v < target)
+                {
+                    right = mid - 1;
                 }
-                else{
-                    left=mid+1;
+                else
+                {
+                    left = mid + 1;
                 }
-                ans=min(ans,abs(target-v));
+                ans = min(ans, abs(target - v));
             }
         }
         return ans;

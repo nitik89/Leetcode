ce3c7fe11c21ea22f16ca66ed68725e76bf1f3d3
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-11-26 03:42:44 +0530

    Time: 638 ms (80.37%), Space: 138.6 MB (49.14%) - LeetHub

--- 2440-create-components-with-same-value/2440-create-components-with-same-value.cpp
+++ 2440-create-components-with-same-value/2440-create-components-with-same-value.cpp
@@ -1,30 +1,39 @@
-class Solution {
-public:
-    int dfs(vector<vector<int>>&vec,vector<int>&nums,int x,int parent,int tar){
-        
-        int sum=nums[x];
-        for(auto y:vec[x]){
-            if(parent==y) continue;
-            sum+=dfs(vec,nums,y,x,tar);
+class Solution
+{
+    public:
+        int dfs(vector<vector < int>> &vec, vector< int > &nums, int x, int parent, int tar)
+        {
+
+            int sum = nums[x];
+            for (auto y: vec[x])
+            {
+                if (parent == y) continue;
+                sum += dfs(vec, nums, y, x, tar);
+            }
+            if (sum == tar) return 0;
+            return sum;
         }
-        if(sum==tar) return 0;
-        return sum;
-    }
-    int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
-        int sum=0,n=nums.size();
-        for(auto x:nums){
-            sum+=x;
+    int componentValue(vector<int> &nums, vector<vector< int>> &edges)
+    {
+        int sum = 0, n = nums.size();
+        for (auto x: nums)
+        {
+            sum += x;
         }
-        vector<vector<int>>vec(n+1);
-        for(auto x:edges){
+        vector<vector < int>> vec(n + 1);
+        for (auto x: edges)
+        {
             vec[x[0]].push_back(x[1]);
             vec[x[1]].push_back(x[0]);
         }
-        int ans=0;
-        for(int i=sum;i>=1;i--){
-            if(sum%i==0){
-                if(!dfs(vec,nums,0,-1,sum/i)){
-                    ans=max(ans,i-1);
+        int ans = 0;
+        for (int i = sum; i >= 1; i--)
+        {
+            if (sum % i == 0)
+            {
+                if (!dfs(vec, nums, 0, -1, sum / i))
+                {
+                    ans = max(ans, i - 1);
                 }
             }
         }

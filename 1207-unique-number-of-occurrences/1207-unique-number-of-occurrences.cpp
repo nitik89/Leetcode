5a821efddb86a0961ac64c85d8c8329bee803420
nitik89 <82098445+nitik89@users.noreply.github.com>
2022-11-30 11:23:13 +0530

    Time: 9 ms (26.75%), Space: 8.2 MB (40.60%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,22 +1,27 @@
-class Solution {
-public:
-    bool uniqueOccurrences(vector<int>& arr) {
-       unordered_map<int,int>mp;
-        unordered_map<int,int>mp1;
-        for(auto x:arr){
-            mp[x]++;
-        }
-     
-        
-       for(auto x:mp){
-           mp1[x.second]++;
-       }
-        for(auto x:mp1){
-            if(x.second>1){
-                
-                return false;
+class Solution
+{
+    public:
+        bool uniqueOccurrences(vector<int> &arr)
+        {
+            unordered_map<int, int> mp;
+            unordered_map<int, int> mp1;
+            for (auto x: arr)
+            {
+                mp[x]++;
             }
+
+            for (auto x: mp)
+            {
+                mp1[x.second]++;
+            }
+            for (auto x: mp1)
+            {
+                if (x.second > 1)
+                {
+
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
-    }
 };
